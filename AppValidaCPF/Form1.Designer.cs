3c6e1d90b0f74b54eae1b9d7cb905d2df66be919
PedroMareg <145200628+PedroMareg@users.noreply.github.com>
2024-04-20 14:11:27 -0300

    Add files via upload

---
+++ AppValidaCPF/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace AppValidaCPF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnValida = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mtbCPF = new System.Windows.Forms.MaskedTextBox();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnValida
+            // 
+            this.btnValida.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btnValida.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnValida.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnValida.ForeColor = System.Drawing.Color.White;
+            this.btnValida.Location = new System.Drawing.Point(384, 48);
+            this.btnValida.Name = "btnValida";
+            this.btnValida.Size = new System.Drawing.Size(205, 57);
+            this.btnValida.TabIndex = 0;
+            this.btnValida.Text = "VALIDAR";
+            this.btnValida.UseVisualStyleBackColor = false;
+            this.btnValida.Click += new System.EventHandler(this.btnValida_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CPF";
+            // 
+            // mtbCPF
+            // 
+            this.mtbCPF.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mtbCPF.Location = new System.Drawing.Point(39, 54);
+            this.mtbCPF.Mask = "000,000,000-99";
+            this.mtbCPF.Name = "mtbCPF";
+            this.mtbCPF.Size = new System.Drawing.Size(291, 30);
+            this.mtbCPF.TabIndex = 2;
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.BackColor = System.Drawing.Color.Red;
+            this.btnFechar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFechar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFechar.ForeColor = System.Drawing.Color.White;
+            this.btnFechar.Location = new System.Drawing.Point(384, 125);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(205, 57);
+            this.btnFechar.TabIndex = 3;
+            this.btnFechar.Text = "FECHAR";
+            this.btnFechar.UseVisualStyleBackColor = false;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(627, 210);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.mtbCPF);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnValida);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Validação de CPF";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnValida;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox mtbCPF;
+        private System.Windows.Forms.Button btnFechar;
+    }
+}
+
---
+++ AppValidaCPF/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppValidaCPF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = mtbCPF;
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(1);
+        }
+
+        private void btnValida_Click(object sender, EventArgs e)
+        {
+            var cpf = ValidaCPF.IsCpf(mtbCPF.Text);
+            if (cpf == true)
+            {
+                MessageBox.Show("CPF Válido!");
+            }
+            else
+            {
+                MessageBox.Show("CPF inválido!");
+            }
+        }
+    }
+}
